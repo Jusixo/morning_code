cf5e7a8a363fa12d31a92432b95a9cf4f37a99c1
Justin Valentin <Justinvalentine88@gmail.com>
2018-03-07 10:47:49 -0500

    bit counting kata complete

---
+++ bit_counting.rb
@@ -0,0 +1,10 @@
+# Write a function that takes an (unsigned) integer as input, and returns the number of bits
+# that are equal to one in the binary representation of that number.
+#
+# Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+
+# Solution:
+def count_bits(n)
+  n.to_s(2).chars.count("1")
+end
