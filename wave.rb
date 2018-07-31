34771e1c824fe51c8dace5111a858e5203382fb4
Justin Valentin <Justinvalentine88@gmail.com>
2018-07-31 00:04:47 -0400

    kata complete

---
+++ wave.rb
@@ -0,0 +1,13 @@
+def wave(str)
+  result = []
+  (0...str.length).each{|c|
+    if str[c] == " "
+    else
+      b = str.split("")
+      a = str[c].capitalize
+      b.delete_at(c)
+        result.push(b.join("").insert(c,a))
+    end
+  }
+  return result
+end
